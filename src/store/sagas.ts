34d569cd0872fe9ae71be0f34bdf54940ad4d18a
Javed Ahmad <106674476+JavedJutt@users.noreply.github.com>
2023-08-18 18:25:33 +0500

    Revert "Todo List with Redux-Saga"

--- src/store/sagas.ts
+++
@@ -1,8 +0,0 @@
-import { all } from "redux-saga/effects";
-import watchFetchTodos from "@/store/TodoList/todoSagas";
-
-function* rootSaga() {
-  yield all([watchFetchTodos()]);
-}
-
-export default rootSaga;
